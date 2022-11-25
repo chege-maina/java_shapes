7155da016e8fa617c89a4088a01d54e9db8188f5
chege maina <mr.shei8@gmail.com>
2022-11-25 09:30:57 +0300

    Change Class name

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         int width = 1000;
